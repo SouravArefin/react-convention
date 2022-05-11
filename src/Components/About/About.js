7ea7c231bbb928a274fd8f4de8930225bc8e4637
Sourav123 <khansourav58@gmail.com>
2022-05-12 00:56:16 +0600

    navbar-about-edit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,14 +4,18 @@ import promi from '../../promi.jpg'
 import rahat from '../../rahat.jpg'
 import asraf from '../../asraf.jpg'
 import sanjid from '../../sanjd.jpg'
+import Type from '../Contact/Type';
 const About = () => {
     return (
     <div>
-   
-        <div data-aos="fade-up-left" style={{margin:"20%"}}className="custom-shadow">
+   <h1 className='text-center text-3xl sp-style mt-20'><Type/></h1>
+            <div data-aos="fade-up-left" style={{
+                marginTop: "10%", marginLeft: '20%',
+                marginRight: '20%',
+            
+            }} className="custom-shadow">
                 <div className='pt-2'>
-                    
-                <img style={{marginLeft: '45%'}}className="w-20 mt-5 h-20 rounded-full  "src={sourav} alt=''/>
+                <img className="about-image w-20 mt-5 h-20 rounded-full  "src={sourav} alt=''/>
             <h1 className='text-2xl sp-style text-center mt-5 pt-5' >Sourav <span className='text-3xl'></span> Arefin.
              </h1>
         <p className='text-blue-500 font-bold p-10 text-center sp-style text-2xl'>Web Developer</p>  
@@ -21,7 +25,7 @@ const About = () => {
 
             <div data-aos="fade-right" style={{ margin: "20%" }} className="custom-shadow">
            <div className='pt-2'>
-           <img style={{marginLeft: '45%'}}className="w-20 mt-5 h-20 rounded-full  "src={promi} alt=''/>
+           <img className="w-20 about-image mt-5 h-20 rounded-full  "src={promi} alt=''/>
             <h1 className='text-2xl sp-style text-center mt-5 pt-5' > <span className='text-3xl'>Promi</span> Chowdhury
              </h1>
 
@@ -32,7 +36,7 @@ const About = () => {
             <div data-aos="fade-down-right" style={{ margin: "20%" }} className="custom-shadow">
                 
        <div className='pt-2'>
-       <img style={{marginLeft: '45%'}}className="w-20 h-20 rounded-full  "src={rahat} alt=''/>
+       <img className="w-20 h-20 about-image rounded-full  "src={rahat} alt=''/>
             <h1 className='text-2xl sp-style text-center mt-5 pt-5' > <span className='text-3xl'>M.A.T</span> Rahat.
              </h1>
 
@@ -44,7 +48,7 @@ const About = () => {
                 
 
      <div  className='pt-2'>
-     <img style={{marginLeft: '45%'}}className="w-20 h-20 rounded-full  "src={sanjid} alt=''/>
+     <img className="w-20 about-image h-20 rounded-full  "src={sanjid} alt=''/>
             <h1 className='text-2xl sp-style text-center mt-5 pt-5' >Samsul <span className='text-3xl'></span> Anwar
              </h1>
 
@@ -55,7 +59,7 @@ const About = () => {
             <div data-aos="flip-down" style={{ margin: "20%" }} className="custom-shadow">
                 
                 <div className='pt-2'>
-                <img style={{marginLeft: '45%'}}className="w-20 h-20 rounded-full  "src={asraf} alt=''/>
+                <img className="w-20 about-image h-20 rounded-full  "src={asraf} alt=''/>
             <h1 className='text-2xl sp-style text-center mt-5 pt-5' ><span className='text-3xl '>Asraful</span> Hossain
              </h1>
 
