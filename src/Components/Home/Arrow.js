adcafc9b79bb5d52b6b9c7cac0ff07b569ba09ed
Sourav123-sys <khansourav58@gmail.com>
2022-08-04 10:14:43 -0700

    msngr-rmv

--- src/Components/Home/Arrow.js
+++ src/Components/Home/Arrow.js
@@ -3,20 +3,21 @@ import upperArrow from '../../upper_arrow_512x512.png'
 import message from '../../messenger.png'
 const Arrow = () => {
     return (
-        <div className="z-50 flex justify-between mt-10 mb-10 mr-10 ">
+        <div className="z-50   mt-10 mb-10 mr-10  text-right">
         
-        <button><a href="https://www.facebook.com/One-Million-101069885894174" target='_blank' >
+            {/* <button>
+                <a href="https://www.facebook.com/One-Million-101069885894174" target='_blank' > */}
             {/* <FontAwesomeIcon
                style={{borderRadius:"50px"}}
             className='text-4xl bg-cyan-900 text-white p-5 ' icon={faArrowUp} /> */}
-        <img src={message} alt="arrow" className="w-44 "/>
-            </a></button>
+        {/* <img src={message} alt="arrow" className="w-44 "/>
+            </a></button> */}
 
             <button><a href="#">
             {/* <FontAwesomeIcon
                style={{borderRadius:"50px"}}
             className='text-4xl bg-cyan-900 text-white p-5 ' icon={faArrowUp} /> */}
-        <img src={upperArrow} alt="arrow" className="w-10 h-10"/>
+        <img src={upperArrow} alt="arrow" className="w-10 h-10 "/>
             </a></button>
      
             
